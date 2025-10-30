2dde1687a58a2fd0211d0a7f9dfa62f1fd73bb68
marlowvious-alt <marlowvious@gmail.com>
2025-10-30 13:03:38 +0000

    Disable credentials for guest access

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,9 @@ import axios from 'axios'
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_REACT_APP_API_URL || '',
-  withCredentials: true,
+withCredentials: false,
+
+
 })
 
 export const signUp = (data) => api.post('/auth/create-user', data)
